616fdc5888122f5b9283f77dd0137042f3a7b3d2
mauriciovillam <mecaurio11@gmail.com>
2020-06-13 22:25:48 -0300

    Remove 'old-src' (code prior restructuring)

--- old-src/bootstrapper/packages/composer.ts
+++
@@ -1,9 +0,0 @@
-import Package from './package'
-
-export default class ComposerPackage extends Package {
-  name = 'geerlingguy.composer'
-
-  get configuration(): object {
-    return {}
-  }
-}
--- old-src/bootstrapper/packages/repo-remi.ts
+++
@@ -1,9 +0,0 @@
-import Package from './package'
-
-export default class RemiRepositoryPackage extends Package {
-  name = 'geerlingguy.repo-remi'
-
-  conditional(): string {
-    return "ansible_os_family == 'RedHat'"
-  }
-}
